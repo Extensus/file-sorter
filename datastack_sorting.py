ca53bb084f9a8e8161f5133407bf0bb25b7cace0
Eric Jaden Stautmeister <eric@stautmeister.ch>
2021-03-10 20:42:26 +0100

    init

---
+++ datastack_sorting.py
@@ -0,0 +1,105 @@
+import os
+
+
+class databaseApi:
+
+    def file_maintainance(self):
+        isfile = os.path.isfile("./file_sort_var_db.txt") and os.access("./file_sort_var_db.txt", os.R_OK)
+        if not isfile:
+            with open("./file_sort_var_db.txt", "w+") as f:
+                f.write("<=#=#=#=> SETTINGS <=#=#=#=>"
+
+                        "\n\n#> EXTENSION LIST (ADD IF YOU NEED MORE, KEEP EXISTING FORMAT) <#"
+                        "\n\nAudio Extensions=['mp3', 'wav', 'raw', 'mid', 'midi', 'wma']"
+                        "\nVideo Extensions=['mp4', 'mpg', 'mpeg', 'avi', 'flv', 'mov', 'mkv', 'mwv', 'h264', 'm4v']"
+                        "\nImage Extensions=['png', 'jpeg', 'jpg', 'gif', 'svg', 'bmp', 'psd', 'tiff', 'tif']"
+                        "\nDocument Extensions=['txt', 'pdf', 'csv', 'xls', 'xlsx', 'ods', 'doc', 'docx', 'tex', "
+                        "'odt', 'ppt', "
+                        "'pptx', 'log'] "
+                        "\nCompressed File Extensions=['zip', 'z', '7z', 'rar', 'tar', 'gz', 'rpm', 'pkg', 'deb']"
+                        "\nExecutable Extensions=['dmg', 'iso', 'exe']"
+                        "\nWord Extensions=['doc', 'docx']"
+                        "\nPower Point Extensions=['ppt', 'pptx', 'pptm', 'ppsx']"
+
+                        "\n\n#> SUPPORTED DIRECTORIES TO SORT TO <#"
+                        "\n\nDownloads Directory=C:\\Users\\eric\\Downloads"
+                        "\nDocuments Directory=C:\\Users\\usr\\Documents"
+                        "\nDesktop Directory=C:\\Users\\usr\\Desktop"
+                        "\nMusic Directory=C:\\Users\\usr\\Music"
+                        "\nVideo Directory=C:\\Users\\usr\\Video"
+                        "\n\Pictures Directory=C:\\Users\\usr\\Pictures"
+                        "\nZip File Directory=C:\\Users\\usr\\Desktop"
+                        "\nExecutable Directory=C:\\Users\\usr\\Desktop"
+
+                        "\n\n#> MONITORED DIRECTORIES THAT ARE SUPPORTED BY THE SYSTEM (Please provide Directories "
+                        "above) <# "
+                        "\n\nSort Downloads=False"
+                        "\nSort Documents=False"
+                        "\nSort Desktop=False"
+                        "\nSort Audio=False"
+                        "\nSort Video=False"
+                        )
+        '''elif isfile:
+            with open("./file_sort_var_db.txt", "r+") as f:
+                content_test = f.readlines()
+                content_test = set(content_test)
+                if content_test == set():
+                    f.write("Success!!")
+                else:
+                    pass
+        else:
+            pass'''
+
+    def __init__(self):
+        self.content = None
+        self.fetchedVars = {}
+        self.vars = []
+        self.conts = []
+
+        self.file_maintainance()
+
+    def variable_fetching(self, item):
+        try:
+            var = None
+            cont = None
+            var = item[0]
+            cont = item[1]
+            self.vars.append(var)
+            self.conts.append(cont)
+        except IndexError:
+            pass
+        return var, cont
+
+    def get_from_base(self):
+        self.file_maintainance()
+        with open('./file_sort_var_db.txt', 'r+') as f:
+            self.content = f.read().splitlines()
+            for item in self.content:
+                list_item = item.split("=", 1)
+                var, cont = self.variable_fetching(list_item)
+                self.fetchedVars[var] = cont
+
+            return self.fetchedVars
+
+    def change_var(self, var):
+
+        pass
+
+    def write_to_base(self):
+        with open('./file_sort_var_db.txt', 'w') as f:
+            try:
+                if self.content is not None:
+                    for x in self.fetchedVars:
+                        f.write(f"{x}\n")
+                else:
+                    pass
+            except TypeError:
+                pass
+
+
+if __name__ == '__main__':
+    api = databaseApi()
+    api.get_from_base()
+    print(api.content)
+    print(api.fetchedVars)
+    api.write_to_base()
---
+++ file_sorter_class.py
@@ -0,0 +1,209 @@
+import collections
+from pprint import pprint
+from datastack_sorting import *
+
+
+class file_sorter_class(object):
+    api = databaseApi()
+    fetchedVars = api.get_from_base()
+    pprint(fetchedVars)
+
+    EXT_AUDIO = fetchedVars['Audio Extensions']
+    EXT_VIDEO = fetchedVars['Video Extensions']
+    EXT_IMAGES = fetchedVars['Image Extensions']
+    EXT_DOCS = fetchedVars['Document Extensions']
+    EXT_COMPRESSED = fetchedVars['Compressed File Extensions']
+    EXT_INSTALLATIONS = fetchedVars['Executable Extensions']
+
+    EXT_WORD = fetchedVars['Word Extensions']
+    EXT_PPP = fetchedVars['Power Point Extensions']
+
+    DOWN_DIR = fetchedVars['Downloads Directory']
+    DOC_DIR = fetchedVars['Documents Directory']
+    DESK_DIR = fetchedVars['Desktop Directory']
+    MUSIC_DIR = fetchedVars['Music Directory']
+    VIDEO_DIR = fetchedVars['Video Directory']
+
+    image_dir = fetchedVars['Pictures Directory']
+    compressed_dir = fetchedVars['Zip File Directory']
+    exe_dir = fetchedVars['Executable Directory']
+
+    down_file_mapping = fetchedVars['Sort Downloads']
+    down_file_map = collections.defaultdict(list)
+    doc_file_mapping = fetchedVars['Sort Documents']
+    doc_file_map = collections.defaultdict(list)
+    desktop_file_mapping = fetchedVars['Sort Desktop']
+    desktop_file_map = collections.defaultdict(list)
+    audio_file_mapping = fetchedVars['Sort Audio']
+    audio_file_map = collections.defaultdict(list)
+    video_file_mapping = fetchedVars['Sort Video']
+    video_file_map = collections.defaultdict(list)
+
+    def map_files(self):
+        try:
+            if self.down_file_mapping:
+                file_list = os.listdir(self.DOWN_DIR)
+                for file_name in file_list:
+                    if file_name[0] != '.':
+                        file_ext = file_name.split('.')[-1]
+                        self.down_file_map[file_ext].append(file_name)
+
+            if self.doc_file_mapping:
+                file_list = os.listdir(self.DOC_DIR)
+                for file_name in file_list:
+                    if file_name[0] != '.':
+                        file_ext = file_name.split('.')[-1]
+                        self.doc_file_map[file_ext].append(file_name)
+
+            if self.desktop_file_mapping:
+                file_list = os.listdir(self.DESK_DIR)
+                for file_name in file_list:
+                    if file_name[0] != '.':
+                        file_ext = file_name.split('.')[-1]
+                        self.desktop_file_map[file_ext].append(file_name)
+
+            if self.audio_file_mapping:
+                file_list = os.listdir(self.MUSIC_DIR)
+                for file_name in file_list:
+                    if file_name[0] != '.':
+                        file_ext = file_name.split('.')[-1]
+                        self.audio_file_map[file_ext].append(file_name)
+
+            if self.video_file_mapping:
+                file_list = os.listdir(self.VIDEO_DIR)
+                for file_name in file_list:
+                    if file_name[0] != '.':
+                        file_ext = file_name.split('.')[-1]
+                        self.video_file_map[file_ext].append(file_name)
+
+        except IndexError:
+            print(f"No such directory (root = %s" % self.DOWN_DIR)
+
+    def send_files_to_dirs(self):
+        try:
+            if self.down_file_mapping:
+                for f_ext, f_list in self.down_file_mapping.items():
+                    if f_ext in self.EXT_AUDIO:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.MUSIC_DIR, file))
+
+                    elif f_ext in self.EXT_VIDEO:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.VIDEO_DIR, file))
+
+                    elif f_ext in self.EXT_IMAGES:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.image_dir, file))
+
+                    elif f_ext in self.EXT_DOCS:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.DOC_DIR, file))
+
+                    elif f_ext in self.EXT_COMPRESSED:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.compressed_dir, file))
+
+                    elif f_ext in self.EXT_INSTALLATIONS:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.exe_dir, file))
+
+                    else:
+                        pass
+            else:
+                pass
+
+            if self.desktop_file_mapping:
+                for f_ext, f_list in self.desktop_file_map.items():
+                    if f_ext in self.EXT_AUDIO:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.MUSIC_DIR, file))
+
+                    elif f_ext in self.EXT_VIDEO:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.VIDEO_DIR, file))
+
+                    elif f_ext in self.EXT_IMAGES:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.image_dir, file))
+
+                    elif f_ext in self.EXT_DOCS:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.DOC_DIR, file))
+
+                    elif f_ext in self.EXT_COMPRESSED:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.compressed_dir, file))
+
+                    elif f_ext in self.EXT_INSTALLATIONS:
+                        for file in f_list:
+                            os.rename(os.path.join(self.DOWN_DIR, file), os.path.join(self.exe_dir, file))
+
+                    else:
+                        pass
+            else:
+                pass
+
+            if self.doc_file_mapping:
+                for f_ext, f_list in self.doc_file_mapping.items():
+                    if f_ext == 'pdf':
+                        for file in f_list:
+                            destination = "PDF\\%s" % file
+                            os.rename(os.path.join(self.DOC_DIR, file), os.path.join(self.DOC_DIR, destination))
+
+                    elif f_ext in self.EXT_WORD:
+                        for file in f_list:
+                            destination = "Word\\%s" % file
+                            os.rename(os.path.join(self.DOC_DIR, file), os.path.join(self.DOC_DIR, destination))
+
+                    elif f_ext == 'txt':
+                        for file in f_list:
+                            destination = "TXT\\%s" % file
+                            os.rename(os.path.join(self.DOC_DIR, file), os.path.join(self.DOC_DIR, destination))
+
+                    else:
+                        pass
+            else:
+                pass
+
+            if self.audio_file_mapping:
+                for f_ext, f_list in self.audio_file_map.items():
+                    if f_ext in self.EXT_AUDIO:
+                        for file in f_list:
+                            destination = f"Audio\\%s" % file
+                            os.rename(os.path.join(self.MUSIC_DIR, file), os.path.join(self.MUSIC_DIR, destination))
+
+                    elif f_ext in self.EXT_VIDEO:
+                        for file in f_list:
+                            destination = f"Video\\%s" % file
+                            os.rename(os.path.join(self.VIDEO_DIR, file), os.path.join(self.VIDEO_DIR, destination))
+
+                    else:
+                        pass
+            else:
+                pass
+            
+            if self.video_file_mapping:
+                for f_ext, f_list in self.video_file_map.items():
+                    if f_ext in self.EXT_AUDIO:
+                        for file in f_list:
+                            destination = f"Audio\\%s" % file
+                            os.rename(os.path.join(self.MUSIC_DIR, file), os.path.join(self.MUSIC_DIR, destination))
+
+                    elif f_ext in self.EXT_VIDEO:
+                        for file in f_list:
+                            destination = f"Video\\%s" % file
+                            os.rename(os.path.join(self.VIDEO_DIR, file), os.path.join(self.VIDEO_DIR, destination))
+
+                    else:
+                        pass
+            else:
+                pass
+
+        except FileExistsError:
+            pass
+
+
+if __name__ == '__main__':
+    dir_sorting = file_sorter_class()
+
+    print(dir_sorting.EXT_AUDIO)
