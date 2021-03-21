e9b6bea4e8b4dba17fecf57c8e31e998f75b7025
Eric Jaden Stautmeister <eric@stautmeister.ch>
2021-03-21 22:21:27 +0100

    -

--- tree_dependency_class.py
+++
@@ -1,9 +0,0 @@
-class Root(object):
-    def __init__(self):
-        self.left = None
-        self.right = None
-
-
-class Node(object):
-    def __init__(self, left=None, right=None):
-        pass
